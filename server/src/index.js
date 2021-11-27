e7c25e596a401c5c75a7ef181bd6f79192e7e879
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-27 16:57:43 +0800

    build track query

--- server/src/index.js
+++ server/src/index.js
@@ -28,6 +28,6 @@ server.listen().then(() => {
   console.log(`
     Server is running!
     Listening on port 4000
-    Query at localhost:4000
+    Query at https://studio.apollographql.com/dev
   `);
 });
