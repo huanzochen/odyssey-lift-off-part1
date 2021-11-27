59bd249490cb686476f3055cb68d9342f705b770
tzuchuan.huang <tzuchuan.huang@rakuten.com>
2021-11-27 21:22:52 +0800

    add Query Result

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Layout } from "../components";
 
-import { gql } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
+
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
 
 export const TRACKS = gql`
   query GetTracks {
@@ -25,7 +28,20 @@ export const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  // if (loading) return "loading...";
+  // if (error) return `Error ${error.message}`;
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track}></TrackCard>
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
